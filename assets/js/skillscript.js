c449dfff46baab80f45ed503c363e66fe74178b2
Pranav Natekar <pranavnat24@gmail.com>
2019-07-26 03:06:11 +0530

    changed radar to bar

--- assets/js/skillscript.js
+++ assets/js/skillscript.js
@@ -1,207 +1,59 @@
-var marksCanvas = document.getElementById("myChart");
 
 Chart.defaults.global.defaultFontFamily = "sans-serif";
 Chart.defaults.global.defaultFontSize = 10;
-// Chart.defaults.global.legend.display = false;
 
-var marksData = {
-	labels: ["Python", "C", "C++", "Matlab"],
-	datasets: [{
-		label: "Software Development Skills",
-		backgroundColor: "rgba(245,106,106,0.3)",
-		data: [90, 70, 60, 95],
-		pointHoverRadius: 10
-	}]
-};
 
-var chartOptions = {
-	scale: {
-		ticks: {
-			beginAtZero: true,
-			min: 0,
-			max: 100,
-			stepSize: 20
-		},
-		pointLabels: {
-			fontSize: 15
-		}
-	},
-	legend: {
-		display:false,
-		position: 'left'
-	},
-	tooltips: {
-		callbacks: {
-			label: function (tooltipItem, data) {
-				var label = data.datasets[tooltipItem.datasetIndex].label || '';
 
-				if (label) {
-					label += ': ';
-				}
-				label += Math.round(tooltipItem.yLabel * 100) / 100;
-				return label;
+new Chart(document.getElementById("myChart"), {
+	type: 'horizontalBar',
+	data: {
+		labels: ["Python", "C", "C++", "Matlab",
+			"HTML", "CSS", "JavaScript",
+			"Git", "CLI", "Latex",
+			"Jupyter", "Tensorflow/Keras/PyTorch", "Scikit-learn", "openCV/NLTK", "Numpy/Scipy/Pandas"],
+		datasets: [
+			{
+				label: "Skill",
+				backgroundColor: ["rgba(200,0,0,0.5)", "rgba(200,0,0,0.5)", "rgba(200,0,0,0.5)", "rgba(200,0,0,0.5)",
+					"rgba(0,200,0,0.5)", "rgba(0,200,0,0.5)", "rgba(0,200,0,0.5)",
+					"rgba(0,0,200,0.5)", "rgba(0,0,200,0.5)", "rgba(0,0,200,0.5)",
+					"rgba(0,200,200,0.5)", "rgba(0,200,200,0.5)", "rgba(0,200,200,0.5)", "rgba(0,200,200,0.5)", "rgba(0,200,200,0.5)"],
+				data: [90, 70, 60, 80,
+					50, 55, 70,
+					80, 85, 50,
+					85, 70, 65, 75, 85]
 			}
-		}
-	}
-};
+		]
+	},
+	options: {
+		legend: { display: false },
+		title: {
+			display: true,
+			text: 'Software Development, Web Development, Tools, Data Science, Hardware and Circuit Skills'
+		},
+		scales: {
+			xAxes: [{
+				gridLines: {
+					display: false
+				},
+				ticks: {
+					display: false,
+					beginAtZero: true,
+					min: 0,
+					max: 100,
+					stepSize: 20,
+				}
+			}],
+			yAxes: [{
+				gridLines: {
+					display: false
+				}
+			}
 
-var radarChart = new Chart(marksCanvas, {
-	type: 'radar',
-	data: marksData,
-	options: chartOptions
+			]
+		}
+
+	}
 });
 
-///////////////////////////////////////////////////////////////////////////////
 
-var marksCanvas = document.getElementById("myChart2");
-
-var marksData = {
-	labels: ["Git", "Arduino", "Latex", "CLI", "Image / Vector Editors"],
-	datasets: [{
-		label: " Tools Skill",
-		backgroundColor: "rgba(0,200,0,0.3)",
-		data: [80, 90, 60, 70, 50],
-		pointHoverRadius: 10
-	}]
-};
-
-var chartOptions = {
-	scale: {
-		ticks: {
-			beginAtZero: true,
-			min: 0,
-			max: 100,
-			stepSize: 20
-		},
-		pointLabels: {
-			fontSize: 15
-		}
-	},
-	legend: {
-		display:false,
-		position: 'left'
-	},
-	tooltips: {
-		callbacks: {
-			label: function (tooltipItem, data) {
-				var label = data.datasets[tooltipItem.datasetIndex].label || '';
-
-				if (label) {
-					label += ': ';
-				}
-				label += Math.round(tooltipItem.yLabel * 100) / 100;
-				return label;
-			}
-		}
-	}
-};
-
-var radarChart = new Chart(marksCanvas, {
-	type: 'radar',
-	data: marksData,
-	options: chartOptions
-});
-
-/////////////////////////////////////////////////////////////////////////////////
-
-var marksCanvas = document.getElementById("myChart3");
-
-var marksData = {
-	labels: ["HTML", "CSS", "JavaScript"],
-	datasets: [{
-		label: "Web Development Skill",
-		backgroundColor: "rgba(0,200,200,0.3)",
-		data: [60, 70, 80],
-		pointHoverRadius: 10
-	}]
-};
-
-var chartOptions = {
-	scale: {
-		ticks: {
-			beginAtZero: true,
-			min: 0,
-			max: 100,
-			stepSize: 20
-		},
-		pointLabels: {
-			fontSize: 15
-		}
-	},
-	legend: {
-		display:false,
-		position: 'left'
-	},
-	tooltips: {
-		callbacks: {
-			label: function (tooltipItem, data) {
-				var label = data.datasets[tooltipItem.datasetIndex].label || '';
-
-				if (label) {
-					label += ': ';
-				}
-				label += Math.round(tooltipItem.yLabel * 100) / 100;
-				return label;
-			}
-		}
-	}
-};
-
-var radarChart = new Chart(marksCanvas, {
-	type: 'radar',
-	data: marksData,
-	options: chartOptions
-});
-
-///////////////////////////////////////////////////////////////
-
-var marksCanvas = document.getElementById("myChart4");
-
-var marksData = {
-	labels: ["Jupyter/PyCharm", "Numpy/Scipy/Pandas", "Tensorflow/Keras", "GGplot/Matplotlib", "OpenCV/scikit-image", "scikit-learn"],
-	datasets: [{
-		label: "Data Science Skill",
-		backgroundColor: "rgba(200,200,0,0.3)",
-		data: [80, 90, 70, 70, 95, 80],
-		pointHoverRadius: 10
-	}]
-};
-
-var chartOptions = {
-	scale: {
-		ticks: {
-			beginAtZero: true,
-			min: 0,
-			max: 100,
-			stepSize: 20
-		},
-		pointLabels: {
-			fontSize: 15
-		}
-	},
-	legend: {
-		display:false,
-		position: 'left',
-	},
-	tooltips: {
-		callbacks: {
-			label: function (tooltipItem, data) {
-				var label = data.datasets[tooltipItem.datasetIndex].label || '';
-
-				if (label) {
-					label += ': ';
-				}
-				label += Math.round(tooltipItem.yLabel * 100) / 100;
-				return label;
-			}
-		}
-	}
-};
-
-
-
-var radarChart = new Chart(marksCanvas, {
-	type: 'radar',
-	data: marksData,
-	options: chartOptions
-});
